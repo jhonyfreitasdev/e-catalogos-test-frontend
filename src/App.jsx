a662f3b4e8be145eb4b31dcf7e60e95236ce17a1
Jhony Freitas <jhony00._@hotmail.com>
2024-03-06 09:44:34 -0300

    Configurando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
+import { createGlobalStyle } from "styled-components"
+import { AppRoutes } from "./routes/route"
+
 export const App = () => {
-  return (
-    <>
-    </>
-  )
+	return (
+		<>
+			<GlobalStyle />
+			<AppRoutes />
+		</>
+	)
 }
+
+const GlobalStyle = createGlobalStyle`
+	*{
+		font-family: Roboto, Helvetica, 'sans-serif';
+		margin: 0;
+		padding: 0;
+		border: none;
+		outline: none;
+		box-sizing: border-box;
+	}
+
+	a{
+		text-decoration: none;
+		color: #000000;
+	}
+
+	ul{
+		list-style: none;
+	}
+
+	img{
+		max-width: 100%;
+	}
+`
---
+++ src/routes/route.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Purchase } from "../pages/purchase/index.jsx"
+
+export const AppRoutes = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<Purchase />} />
+        </Routes>
+    </BrowserRouter>
+)
