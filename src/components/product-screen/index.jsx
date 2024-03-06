91e9d2b8b85dd8dce8a8b06c4e98df1d2df3e46f
Jhony Freitas <jhony00._@hotmail.com>
2024-03-06 20:37:55 -0300

    Fazendo fetch de dados no componente product screen

--- src/components/product-screen/index.jsx
+++ src/components/product-screen/index.jsx
@@ -1,12 +1,27 @@
+import { useContext, useEffect } from "react";
 import { Header } from "../header";
 import { ProductSlider } from "../product-slider";
 import { ProductInfo } from "../product-info";
 import { ProductRef } from "../product-ref";
 import { ValueContainer } from "../value-container";
 import { ProductPack } from "../product-pack";
+import { DataContext } from "../../context/data-context";
 import { ProductScreenContainer, Line } from './style';
 
 export const ProductScreen = () => {
+    const { setData } = useContext(DataContext)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('./src/data/mock-data.json');
+            const data = await response.json();
+
+            setData(data);
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <ProductScreenContainer>
             <Header />
