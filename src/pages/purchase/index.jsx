21082fa643c3e03ac3a5649f77a780be52646bc5
Jhony Freitas <jhony00._@hotmail.com>
2024-03-06 12:07:33 -0300

    Criando página do projeto

---
+++ src/pages/purchase/index.jsx
@@ -0,0 +1,10 @@
+import { ProductScreen } from "../../components/product-screen"
+import { PurchaseContainer } from "./style"
+
+export const Purchase = () => {
+    return (
+        <PurchaseContainer>
+            <ProductScreen />
+        </PurchaseContainer>
+    )
+}
---
+++ src/pages/purchase/style.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const PurchaseContainer = styled.div `
+    max-width: 360px;
+    height: 100vh;
+    margin: 0 auto;
+    border: 2px solid #000000;
+`
