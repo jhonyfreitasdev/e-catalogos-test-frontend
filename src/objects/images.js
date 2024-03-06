529bd8e3ce2480824a3bf0299836aa55b971ab72
Jhony Freitas <jhony00._@hotmail.com>
2024-03-06 09:43:55 -0300

    Criando objeto com imagens do projeto

---
+++ src/objects/images.js
@@ -0,0 +1,20 @@
+import forwardArrow from "../assets/images/forward-arrow.svg";
+import returnArrow from "../assets/images/return-arrow.svg";
+import addButton from "../assets/images/add-button.svg";
+import removeButton from "../assets/images/remove-button.svg";
+import cart from "../assets/images/cart.svg";
+import equal from "../assets/images/equal.svg";
+import info from "../assets/images/info.svg";
+import query from "../assets/images/query.svg";
+
+
+export const images = {
+    forwardArrow,
+    returnArrow,
+    addButton,
+    removeButton,
+    cart,
+    equal,
+    info,
+    query
+}
