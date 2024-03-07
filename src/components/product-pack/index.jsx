8c01684c6ef28e596450069b6c629f9492d46f94
Jhony Freitas <jhony00._@hotmail.com>
2024-03-07 01:00:18 -0300

    Atualizando Product Pack a partir do Data

--- src/components/product-pack/index.jsx
+++ src/components/product-pack/index.jsx
@@ -1,17 +1,24 @@
+import { DataContext } from "../../context/data-context";
+import { useContext } from "react";
 import { images } from "../../objects/images";
 import { ProductPackContainer, Pack, Size, TotalPack } from "./style";
 
 export const ProductPack = () => {
+    const { data, currentIndex } = useContext(DataContext);
+
+    let totalPack = 0;
+    Object.values(data[currentIndex].sizes).forEach(item => { totalPack += item });
+
     return (
         <ProductPackContainer>
-            <Pack> {2} <Size> P </Size> </Pack>
-            <Pack> {2} <Size> M </Size> </Pack>
-            <Pack> {2} <Size> G </Size> </Pack>
-            <Pack> {2} <Size> GG </Size> </Pack>
+            <Pack> {data[currentIndex].sizes.P} <Size> P </Size> </Pack>
+            <Pack> {data[currentIndex].sizes.M} <Size> M </Size> </Pack>
+            <Pack> {data[currentIndex].sizes.G} <Size> G </Size> </Pack>
+            {data[currentIndex].sizes.GG ? <Pack> {data[currentIndex].sizes.GG} <Size> GG </Size> </Pack> :""}
 
             <img src={images.equal} alt="Sinal de igual" />
 
-            <Pack> {8} <TotalPack> PACK </TotalPack> </Pack>
+            <Pack> {totalPack} <TotalPack> PACK </TotalPack> </Pack>
         </ProductPackContainer>
     )
 }
