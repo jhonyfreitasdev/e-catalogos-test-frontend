98d471020e44b36fee7da543e665b4e617a4a33a
Jhony Freitas <jhony00._@hotmail.com>
2024-03-07 00:44:48 -0300

    Finalizando a lógica de value-container

--- src/components/value-container/index.jsx
+++ src/components/value-container/index.jsx
@@ -1,25 +1,90 @@
+import { useContext, useEffect } from "react";
+import { ShoppingCartContext } from "../../context/shopping-cart";
+import { DataContext } from "../../context/data-context";
 import { images } from "../../objects/images";
 import { Container, Price, Count, Desc } from "./style";
 
 export const ValueContainer = () => {
+    const { shoppingCart, setShoppingCart } = useContext(ShoppingCartContext);
+    const { data, setData, currentIndex } = useContext(DataContext);
+
+    useEffect(() => {updateShoppingCart();}, [data])
+
+    let totalPack = 0;
+    Object.values(data[currentIndex].sizes).forEach(item => { totalPack += item });
+    const packPrice = totalPack * data[currentIndex].price;
+    let currentPrice = 0
+    if (data[currentIndex].quantity > 0) { currentPrice = packPrice * data[currentIndex].quantity }
+
+    const formatPrice = price => price.toFixed(2).replace('.', ',');
+    const addProduct = () => {
+        if (data[currentIndex].quantity) {
+            const newQuantity = data[currentIndex].quantity + 1
+            const newData = data.map(item => {
+                return item.id === data[currentIndex].id
+                    ? { ...data[currentIndex], quantity: newQuantity }
+                    : item
+            });
+            setData(newData);
+        } else {
+            const newData = data.map(item => {
+                return item.id === data[currentIndex].id
+                    ? { ...data[currentIndex], quantity: 1 }
+                    : item
+            });
+            setData(newData);
+        }
+    };
+    const removeProduct = () => {
+        if (data[currentIndex].quantity) {
+            if (data[currentIndex].quantity > 0) {
+                const newData = data.map(item => {
+                    const newQuantity = data[currentIndex].quantity - 1;
+
+                    return item.id === data[currentIndex].id
+                        ? { ...data[currentIndex], quantity: newQuantity }
+                        : item
+                });
+                setData(newData);
+            };
+        };
+    };
+    const updateShoppingCart = () => {
+        const currentPriceList = data.map(item => {
+            let totalPack = 0;
+            Object.values(item.sizes).forEach(item => { totalPack += item });
+            const packPrice = totalPack * item.price;
+            let currentPrice = 0
+            if (item.quantity > 0) {
+                currentPrice = packPrice * item.quantity
+                return currentPrice
+            }
+            return currentPrice
+        })
+        let total = 0;
+        currentPriceList.forEach(item => total += item);
+
+        setShoppingCart({ totalPrice: total })
+    }
+
     return (
         <Container>
             <Price>
                 <Desc>Atual</Desc>
-                <p>{'R$ 0,00'}</p>
+                <p>{`R$ ${formatPrice(currentPrice)}`}</p>
             </Price>
 
             <Count>
-                <img src={images.removeButton} alt="Botão de remover" />
+                <img onClick={removeProduct} src={images.removeButton} alt="Botão de remover" />
 
-                <p> {0} </p>
+                <p> {data[currentIndex].quantity ?? 0} </p>
 
-                <img src={images.addButton} alt="Botão de adicionar" />
+                <img onClick={addProduct} src={images.addButton} alt="Botão de adicionar" />
             </Count>
 
             <Price>
                 <Desc>Acumulado</Desc>
-                <p>{'R$ 0,00'}</p>
+                <p>{`R$ ${formatPrice(shoppingCart.totalPrice)}`}</p>
             </Price>
         </Container>
     )
