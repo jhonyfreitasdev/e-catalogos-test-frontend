1594d71fd2bc75b7f044a5e81283822442598b5b
Jhony Freitas <jhony00._@hotmail.com>
2024-03-07 00:56:46 -0300

    Atualizando Contexto de Shopping Cart para ficar síncrono com contexto de Data

--- src/context/shopping-cart.jsx
+++ src/context/shopping-cart.jsx
@@ -1,12 +1,34 @@
-import { Children, createContext, useState } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
+import { DataContext } from "./data-context";
 
 export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({children}) => {
     const [ shoppingCart, setShoppingCart ] = useState({totalPrice: 0.00});
+    const { data } = useContext(DataContext);
+
+    useEffect(() => {updateShoppingCart();}, [data]);
+
+    const updateShoppingCart = () => {
+        const currentPriceList = data.map(item => {
+            let totalPack = 0;
+            Object.values(item.sizes).forEach(item => { totalPack += item });
+            const packPrice = totalPack * item.price;
+            let currentPrice = 0;
+            if (item.quantity > 0) {
+                currentPrice = packPrice * item.quantity
+                return currentPrice;
+            }
+            return currentPrice;
+        });
+        let total = 0;
+        currentPriceList.forEach(item => total += item);
+
+        setShoppingCart({ totalPrice: total });
+    };
 
     return (
-        <ShoppingCartContext.Provider value={{shoppingCart, setShoppingCart}}>
+        <ShoppingCartContext.Provider value={{shoppingCart}}>
             {children}
         </ShoppingCartContext.Provider>
     )
