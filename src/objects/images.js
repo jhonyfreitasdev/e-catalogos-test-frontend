db0733d78f246049409a5715b0fae5771c3a7aec
Jhony Freitas <jhony00._@hotmail.com>
2024-03-07 17:33:21 -0300

    Adicionando imagens de botões

--- src/objects/images.js
+++ src/objects/images.js
@@ -6,6 +6,9 @@ import cart from "../assets/images/cart.svg";
 import equal from "../assets/images/equal.svg";
 import info from "../assets/images/info.svg";
 import query from "../assets/images/query.svg";
+import whiteForwardArrow from "../assets/images/white-button-forward.svg";
+import whiteReturnArrow from "../assets/images/white-button-return.svg";
+import closeButton from "../assets/images/close-button.svg";
 
 
 export const images = {
@@ -16,5 +19,8 @@ export const images = {
     cart,
     equal,
     info,
-    query
-}
+    query,
+    whiteForwardArrow,
+    whiteReturnArrow,
+    closeButton
+};
