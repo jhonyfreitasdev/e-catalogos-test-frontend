04fb09654822a9dbb364b780156b402befef3d80
Jhony Freitas <jhony00._@hotmail.com>
2024-03-07 01:00:39 -0300

    Atualizando Product Info a partir do Data

--- src/components/product-info/index.jsx
+++ src/components/product-info/index.jsx
@@ -1,21 +1,24 @@
+import { useContext } from "react";
+import { DataContext } from "../../context/data-context";
 import { images } from "../../objects/images";
 import { ProductInfoContainer, ImageSlider, ImageContainer } from "./style"
 
 export const ProductInfo = () => {
-    return ( 
+    const { data, currentIndex } = useContext(DataContext)
+    
+    return (
         <ProductInfoContainer>
             <img src={images.info} alt="Imagem de info" />
             <img src={images.query} alt="Imagem de lupa" />
 
             <ImageSlider>
-                {/* Map com renderização de imagens  */}
-
-                <ImageContainer>
-                    <img src={images.image} alt="Teste" />
-                </ImageContainer>
-                <ImageContainer>
-                    <img src={images.image} alt="Teste" />
-                </ImageContainer>
+                {data[currentIndex].images.map((item, index) => {
+                    return (
+                        <ImageContainer key={index} onClick={() => console.log(index)}>
+                            <img src={item.image} alt="Teste" />
+                        </ImageContainer>
+                    )
+                })}
             </ImageSlider>
 
             <img src={images.cart} alt="Imagem de carrinho de compras" />
