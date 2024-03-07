d36e46cb0651ff2243bdf198675d9083ce2038cb
Jhony Freitas <jhony00._@hotmail.com>
2024-03-07 01:00:59 -0300

    Atualizando Header a partir do Data

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,21 +1,23 @@
+import { useContext } from "react";
 import { images } from "../../objects/images.js";
+import { DataContext } from "../../context/data-context.jsx";
 import { HeaderContainer, ProductSectionContainer, ReturnButton, ProductSection, ForwardButton, FunctionSpan } from "./style.jsx";
 
 export const Header = () => {
+    const { data, currentIndex } = useContext(DataContext);
+
+    const productsInCategory = data.filter(item => item.category_name === data[currentIndex].category_name).length;
+
     return (
         <HeaderContainer>
             <img src={images.returnArrow} alt="Btn de retornar" />
-
             <ProductSectionContainer>
                 <ReturnButton type="button"> &lt; </ReturnButton>
-
                 <div>
-                    <ProductSection>{`(4) Casaco`}</ProductSection>
+                    <ProductSection>{`(${productsInCategory}) ${data[currentIndex].category_name}`}</ProductSection>
                 </div>
-
                 <ForwardButton type="button"> &gt; </ForwardButton>
             </ProductSectionContainer>
-
             <FunctionSpan>F</FunctionSpan>
         </HeaderContainer>
     )
