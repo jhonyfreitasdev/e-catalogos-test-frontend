351ca58d3888789577026be682c04867a6ec305e
Jhony Freitas <jhony00._@hotmail.com>
2024-03-07 23:07:56 -0300

    Refatorando componente

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,45 +1,41 @@
 import { useContext, useState } from "react";
 import { images } from "../../objects/images.js";
 import { DataContext } from "../../context/data-context.jsx";
+
 import { HeaderContainer, ProductSectionContainer, ProductSection, FunctionSpan } from "./style.jsx";
 
 export const Header = () => {
-    const [currentCategoryIndex, setCurrentCategoryIndex] = useState(0);
     const { data, currentIndex, setCurrentIndex } = useContext(DataContext);
-    const categoryList = data.map(item => item.category_name);
-    const productsInCategory = data.filter(item => item.category_name === data[currentIndex].category_name).length;
+    const [currentCategoryIndex, setCurrentCategoryIndex] = useState(0);
 
-    const actualCategory = categoryList[currentIndex];
-    
+    const categoryList = data.map(({category_name}) => category_name);
     const uniqueCategoriesList = [...new Set(categoryList)];
+    const actualCategory = categoryList[currentIndex];
 
+    const productsInCategory = data.filter(item => item.category_name === data[currentIndex].category_name).length;
     
-    const prevSection = () => {
-        setCurrentCategoryIndex((prevIndex) => (prevIndex - 1 + uniqueCategoriesList.length) % uniqueCategoriesList.length);
-        const foundCategory = data.find(item => item.category_name === uniqueCategoriesList[currentCategoryIndex] )
+    const navigateCategory = (direction) => {
+        const newIndex = (currentCategoryIndex + direction + uniqueCategoriesList.length) % uniqueCategoriesList.length;
+        setCurrentCategoryIndex(newIndex);
+        
+        const foundCategory = data.find((item) => item.category_name === uniqueCategoriesList[newIndex]);
         const productIndex = data.indexOf(foundCategory);
-        setCurrentIndex(productIndex)
-    }
-
-    const nextSection = () => {
-        setCurrentCategoryIndex((prevIndex) => (prevIndex + 1) % uniqueCategoriesList.length);
-        const foundCategory = data.find(item => item.category_name === uniqueCategoriesList[currentCategoryIndex] )
-        const productIndex = data.indexOf(foundCategory);
-        setCurrentIndex(productIndex)
-    }
+        setCurrentIndex(productIndex);
+    };
+    
     return (
         <HeaderContainer>
             <img src={images.returnArrow} alt="Btn de retornar" />
 
             <ProductSectionContainer>
-                <img src={images.whiteReturnArrow} onClick={prevSection} alt="Imagem de seta" />
+                <img src={images.whiteReturnArrow} onClick={() => navigateCategory(-1)} alt="Imagem de seta" />
                 <div>
                     <ProductSection>{`(${productsInCategory}) ${actualCategory}`}</ProductSection>
                 </div>
-                <img src={images.whiteForwardArrow} onClick={nextSection} alt="Imagem de seta" />
+                <img src={images.whiteForwardArrow} onClick={() => navigateCategory(1)} alt="Imagem de seta" />
             </ProductSectionContainer>
 
             <FunctionSpan>F</FunctionSpan>
         </HeaderContainer>
-    )
-}
+    );
+};
