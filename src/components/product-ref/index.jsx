8512683796383606705fbdd562a17ac82010512a
Jhony Freitas <jhony00._@hotmail.com>
2024-03-07 21:01:56 -0300

    Refatorando componente

--- src/components/product-ref/index.jsx
+++ src/components/product-ref/index.jsx
@@ -1,20 +1,22 @@
 import { useContext } from "react";
 import { DataContext } from "../../context/data-context";
+
 import { ProductRefContainer, Ref, Name, Code, Price } from "./style";
 
 export const ProductRef = () => {
     const { data, currentIndex } = useContext(DataContext);
 
-    const formattedPrice = data[currentIndex].price.toFixed(2).replace(',', '.');
-    const name = data[currentIndex].name.split(' ')[0];
+    const { name, price, reference } = data[currentIndex]; 
+    const firstName = name.split(' ')[0];
+    const formattedPrice = price.toFixed(2).replace(',', '.');
 
     return (
         <ProductRefContainer>
             <Ref>
-                <Name>{name}</Name>
-                <Code><span> REF: </span> {data[currentIndex].reference}</Code>
+                <Name>{firstName}</Name>
+                <Code><span> REF: </span> {reference}</Code>
                 <Price><span> R$ </span> {formattedPrice}</Price>
             </Ref>
         </ProductRefContainer>
-    )
-}
+    );
+};
