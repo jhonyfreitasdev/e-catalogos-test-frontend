e20235acd24035f64814460d307f1ded6b7523de
Jhony Freitas <jhony00._@hotmail.com>
2024-03-08 00:07:07 -0300

    Refatorando componente

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { images } from "../../objects/images.js";
 import { DataContext } from "../../context/data-context.jsx";
 
@@ -6,21 +6,18 @@ import { HeaderContainer, ProductSectionContainer, ProductSection, FunctionSpan 
 
 export const Header = () => {
     const { data, currentIndex, setCurrentIndex } = useContext(DataContext);
-    const [currentCategoryIndex, setCurrentCategoryIndex] = useState(0);
-
+    
     const categoryList = data.map(({category_name}) => category_name);
     const uniqueCategoriesList = [...new Set(categoryList)];
     const actualCategory = categoryList[currentIndex];
+    const productsInCategory = data.filter(({category_name}) => category_name === data[currentIndex].category_name).length;
+    let currentIndexCategory = uniqueCategoriesList.indexOf(actualCategory);
 
-    const productsInCategory = data.filter(item => item.category_name === data[currentIndex].category_name).length;
-    
-    const navigateCategory = (direction) => {
-        const newIndex = (currentCategoryIndex + direction + uniqueCategoriesList.length) % uniqueCategoriesList.length;
-        setCurrentCategoryIndex(newIndex);
-        
-        const foundCategory = data.find((item) => item.category_name === uniqueCategoriesList[newIndex]);
-        const productIndex = data.indexOf(foundCategory);
-        setCurrentIndex(productIndex);
+    const changeCategory = (direction) => {
+        let nextIndexCategory = (currentIndexCategory + direction + uniqueCategoriesList.length) % uniqueCategoriesList.length;
+        const fistProductOfCategory = data.find(({category_name}) => category_name === uniqueCategoriesList[nextIndexCategory]);
+        const indexProduct = data.indexOf(fistProductOfCategory);
+        setCurrentIndex(indexProduct);
     };
     
     return (
@@ -28,11 +25,11 @@ export const Header = () => {
             <img src={images.returnArrow} alt="Btn de retornar" />
 
             <ProductSectionContainer>
-                <img src={images.whiteReturnArrow} onClick={() => navigateCategory(-1)} alt="Imagem de seta" />
+                <img src={images.whiteReturnArrow} onClick={() => changeCategory(-1)} alt="Imagem de seta" />
                 <div>
                     <ProductSection>{`(${productsInCategory}) ${actualCategory}`}</ProductSection>
                 </div>
-                <img src={images.whiteForwardArrow} onClick={() => navigateCategory(1)} alt="Imagem de seta" />
+                <img src={images.whiteForwardArrow} onClick={() => changeCategory(1)} alt="Imagem de seta" />
             </ProductSectionContainer>
 
             <FunctionSpan>F</FunctionSpan>
